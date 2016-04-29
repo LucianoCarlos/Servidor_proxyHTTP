a5141e1b04c1e7fc626f6819f4f61f00ef922bd7
Luciano Carlos <luciano_krlos@hotmail.com>
2016-04-29 00:08:12 -0300

    Adicionado: fechar arquivos

--- src/filtro_conteudo.py
+++ src/filtro_conteudo.py
@@ -1,32 +1,34 @@
 # coding: utf-8
 from settings import PALAVRAS_BLOQUEADAS, DOMINIOS_BLOQUEADOS
 
+
 def pesquisaPalavra(html):
-	''' Pesquisa palavras dentro de um conteúdo html'''
-	
-	try:
-		arq = open(PALAVRAS_BLOQUEADAS)
-		for palavra in arq:
-			palavra = palavra.replace('\n', '')
-			if palavra in html:
-				print palavra
-				return True
-		return False
-	except IOError:
-		return False
-	
-	
+    ''' Pesquisa palavras dentro de um conteúdo html'''
+    arq = ''
+    try:
+        arq = open(PALAVRAS_BLOQUEADAS)
+        for palavra in arq:
+            palavra = palavra.strip()
+            if palavra and palavra in html:
+                return True
+        return False
+    except IOError:
+        return False
+    finally:
+        arq.close()
+
+
 def verificaDominio(host):
-	'''Verifica se host tem permissão'''
-	
-	try:
-		arq = open(DOMINIOS_BLOQUEADOS)
-		for dominio in arq:
-			dominio = dominio.replace('\n', '')
-			print dominio
-			if dominio == host:
-				return True
-		return False
-	except IOError:
-		return False
-	
+    '''Verifica se host tem permissão'''
+    arq = ''
+    try:
+        arq = open(DOMINIOS_BLOQUEADOS)
+        for dominio in arq:
+            dominio = dominio.strip()
+            if dominio and dominio == host:
+                return True
+        return False
+    except IOError:
+        return False
+    finally:
+        arq.close()
